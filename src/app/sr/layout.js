2aef077a95393ec48f479aeaaad62b0d93158374
Đorđe Popović <djordjepopovicgm@gmail.com>
2026-06-03 12:25:25 +0200

    feat(analytics): add Google Analytics 4 on public pages via @next/third-parties

--- src/app/sr/layout.js
+++ src/app/sr/layout.js
@@ -1,7 +1,10 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import PublicHeader from "@/components/PublicHeader";
 import PublicFooter from "@/components/PublicFooter";
 import ScrollAnimator from "@/components/ScrollAnimator";
 import "@/styles/public-pages.css";
+
+const gaId = process.env.NEXT_PUBLIC_GA_ID;
 
 export default function SrLayout({ children }) {
   return (
@@ -10,6 +13,9 @@ export default function SrLayout({ children }) {
       <ScrollAnimator />
       {children}
       <PublicFooter lang="sr" />
+      {process.env.NODE_ENV === "production" && gaId && (
+        <GoogleAnalytics gaId={gaId} />
+      )}
     </div>
   );
 }
